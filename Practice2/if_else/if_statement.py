2725a40ad876a5efd07efa4b213beb2f74149e0e
dakamba <almdlkm07@gmail.com>
2026-01-30 18:44:15 +0500

    Add comment to if statement for clarity

--- Practice2/if_else/if_statement.py
+++ Practice2/if_else/if_statement.py
@@ -5,7 +5,7 @@ Executes a block only if the condition is True.
 
 temperature = 28
 
-if temperature > 25:
+if temperature > 25: # True
     print("It's warm.")
     print("You might want water.")
 
