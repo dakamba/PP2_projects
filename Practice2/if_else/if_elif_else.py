7c9be178b0dc2d674805dbdf3afcaeb2e89cf866
dakamba <almdlkm07@gmail.com>
2026-01-30 18:43:33 +0500

    Update if_elif_else.py

--- Practice2/if_else/if_elif_else.py
+++ Practice2/if_else/if_elif_else.py
@@ -5,13 +5,13 @@ Multiple conditions, first match wins.
 
 score = 82
 
-if score >= 90:
+if score >= 90: # False
     print("A")
-elif score >= 80:
+elif score >= 80: # True
     print("B")
-elif score >= 70:
+elif score >= 70: # we have not reached this condition
     print("C")
 else:
-    print("D or below")
+    print("D or below") # Execute if none of the conditions are True
 
 # Tip: order matters!
