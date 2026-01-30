ba6c9a70ffd6efff497867df352a27b4382c228e
dakamba <almdlkm.07@gmail.com>
2026-01-30 09:17:44 +0500

    Your commit message

---
+++ Practice1/python-basics/01_basics/variables_and_types.py
@@ -0,0 +1,21 @@
+a = 5
+b = 2.5
+c = "hello"
+d = True
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+
+# variable can change type
+x = 100
+print("x:", x, type(x))
+x = "now string"
+print("x:", x, type(x))
+
+# mini task
+item = "apple"
+count = 3
+price = 120.5
+print("item:", item, "| count:", count, "| price:", price)
