dd62ca6a848d6e40ee2ae0c7e4621e2993ab4896
dakamba <almdlkm07@gmail.com>
2026-02-19 14:23:38 +0500

    Update args_kwargs.py

--- Practice_03/Functions/args_kwargs.py
+++ Practice_03/Functions/args_kwargs.py
@@ -11,6 +11,10 @@ def print_info(**kwargs):
     for key, value in kwargs.items():
         print(f"{key}: {value}")
 
+def together(*args, **kwargs):
+    '''kwargs positions have to be after *args becouse kwargs more importnt than args (args < kwargs by positions)'''
+
 
 print_numbers(1, 2, 3)
 print_info(name="Daulet", age=17)
+
