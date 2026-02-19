89fd1e6c80204fd7b59e542f45d1f562fec5858d
dakamba <almdlkm07@gmail.com>
2026-02-19 14:30:55 +0500

    Update lambda_with_sorted.py

--- Practice_03/Lambda/lambda_with_sorted.py
+++ Practice_03/Lambda/lambda_with_sorted.py
@@ -9,3 +9,8 @@ students = [
 sorted_students = sorted(students, key=lambda x: x["grade"])
 
 print(sorted_students)
+
+# [  {'name': 'Daulet', 'grade': 78}, 
+#    {'name': 'Ali', 'grade': 85}, 
+#    {'name': 'Aruzhan', 'grade': 92}
+# ] 
